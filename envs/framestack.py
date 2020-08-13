410ddc026cd0f7b05ab0e48ac8c3c8bc48efa89a
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-13 13:28:30 -0400

    s

--- envs/framestack.py
+++ envs/framestack.py
@@ -6,5 +6,5 @@ from envs.procgen_env_wrapper import ProcgenEnvWrapper
 # Register Env in Ray
 registry.register_env(
     "stacked_procgen_env",  # This should be different from procgen_env_wrapper
-    lambda config: FrameStack(ProcgenEnvWrapper(config), 8),
+    lambda config: FrameStack(ProcgenEnvWrapper(config), 4),
 )
