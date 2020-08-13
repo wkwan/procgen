019663420e885c782801f32f8fd790c1c2915973
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-13 17:59:09 -0400

    s

--- models/impala_cnn_tf.py
+++ models/impala_cnn_tf.py
@@ -53,8 +53,9 @@ class ImpalaCNN(TFModelV2):
         depths = [16, 32, 32]
         print("the shape is ", obs_space.shape)
         inputs = tf.keras.layers.Input(shape=obs_space.shape, name="observations")
+        print("inputs shape", inputs.shape)
         inputs = tf.reshape(inputs, (-1,64,64,3))
-        print("made the input thing")
+        print("made the input thing", inputs.shape)
 
         # print("inputs shape ", inputs.shape)
         scaled_inputs = tf.cast(inputs, tf.float32) / 255.0
