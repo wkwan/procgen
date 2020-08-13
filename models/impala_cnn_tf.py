e74b20e43bc4ef92911e925517c5220765bf792a
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-13 18:46:48 -0400

    s

--- models/impala_cnn_tf.py
+++ models/impala_cnn_tf.py
@@ -21,7 +21,7 @@ def residual_block(x, depth, prefix):
 def conv_sequence(x, depth, prefix):
     if "seq0" in prefix:
         print("do seq 0")
-        x = conv_layer(depth, prefix + "_conv")(x[0])
+        x = conv_layer(depth, prefix + "_conv")(x)
     else:
         print("do ", prefix)
         x = conv_layer(depth, prefix + "_conv")(x)
