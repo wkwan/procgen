17d4988aeec2d9a777467ed794e13397864fd5f8
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-13 19:18:32 -0400

    s

--- models/impala_cnn_tf.py
+++ models/impala_cnn_tf.py
@@ -23,7 +23,7 @@ def conv_sequence(x, depth, prefix):
     print("x shape", x.shape)
     if "seq0" in prefix:
         print("do seq 0")
-        x = conv_layer(depth, prefix + "_conv")(tf.reshape(x, (None, 256, 64, 3)))
+        x = conv_layer(depth, prefix + "_conv")(tf.reshape(x, (x.shape[0], x.shape[1] * x.shape[2], x.shape[3], x.shape[4])))
     else:
         print("do ", prefix)
         x = conv_layer(depth, prefix + "_conv")(x)
