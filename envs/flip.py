8d836e3179ab15a636955e1d0d17abdf27703ce1
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-17 20:28:51 -0400

    s

--- envs/flip.py
+++ envs/flip.py
@@ -21,7 +21,7 @@ class Cutout(object):
         
     def do_augmentation(self, img):
         cut_img = img.copy()
-        for i, (w11, h11) in enumerate(zip([self.w1, self.h1])):
+        for (w11, h11) in np.array(list(zip(self.w1,self.w2))):
             cut_img[self.pivot_h+h11:self.pivot_h+h11+h11, 
                     self.pivot_w+w11:self.pivot_w+w11+w11, :] = 0
         return cut_img
