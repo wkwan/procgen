20375a18fb4d0ca55ce0b62caba7af8fcd6bbdf6
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-17 20:54:06 -0400

    s

--- envs/flip.py
+++ envs/flip.py
@@ -5,37 +5,6 @@ from envs.procgen_env_wrapper import ProcgenEnvWrapper
 import numpy as np
 from gym import ObservationWrapper
 
-class Cutout(object):
-    def __init__(self, 
-                 box_min=7, 
-                 box_max=22, 
-                 pivot_h=12, 
-                 pivot_w=24):
-        
-        self.box_min = box_min
-        self.box_max = box_max
-        self.pivot_h = pivot_h
-        self.pivot_w = pivot_w
-        self.w1 = np.random.randint(self.box_min, self.box_max)
-        self.h1 = np.random.randint(self.box_min, self.box_max)
-        
-    def do_augmentation(self, img):
-        cut_img = img.copy()
-        print(self.w1)
-        print(self.h1)
-        # for i, (w11, h11) in enumerate(zip(self.w1, self.h1)):
-        #     cut_img[self.pivot_h+h11:self.pivot_h+h11+h11, 
-        #             self.pivot_w+w11:self.pivot_w+w11+w11, :] = 0
-        return cut_img
-    
-    def change_randomization_params(self, index_):
-        self.w1[index_] = np.random.randint(self.box_min, self.box_max)
-        self.h1[index_] = np.random.randint(self.box_min, self.box_max)
-
-    def change_randomization_params_all(self):
-        self.w1 = np.random.randint(self.box_min, self.box_max)
-        self.h1 = np.random.randint(self.box_min, self.box_max)
-
 class Flip(ObservationWrapper):
     def __init__(self, env_wrapper):
         super().__init__(env_wrapper)
@@ -46,10 +15,18 @@ class Flip(ObservationWrapper):
 
     def step(self, action):
         if self.prev_obs is not None:
+            
             # obs = np.flipud(self.prev_obs)
-            cutout_maker = Cutout()
-            obs = cutout_maker.do_augmentation(self.prev_obs)
-            print("did obs")
+            box_min = 7
+            box_max = 22
+            pivot_h = 12
+            pivot_w = 24
+            w1 = np.random.randint(self.box_min, self.box_max)
+            h1 = np.random.randint(self.box_min, self.box_max)
+
+            self.prev_obs[self.pivot_h+h11:self.pivot_h+h11+h11, 
+                    self.pivot_w+w11:self.pivot_w+w11+w11, :] = 0
+            obs = self.prev_obs
             reward = self.prev_reward
             done = self.prev_done
             info = self.prev_info
