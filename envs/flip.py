1070353bc2db46524f8fbbb34bc3b3cc1180c244
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-18 13:42:04 -0400

    s

--- envs/flip.py
+++ envs/flip.py
@@ -5,6 +5,9 @@ from envs.procgen_env_wrapper import ProcgenEnvWrapper
 import numpy as np
 from gym import ObservationWrapper
 
+from skimage.util.shape import view_as_windows
+from skimage.transform import resize
+
 class Flip(ObservationWrapper):
     def __init__(self, env_wrapper):
         super().__init__(env_wrapper)
@@ -16,17 +19,38 @@ class Flip(ObservationWrapper):
     def step(self, action):
         if self.prev_obs is not None:
             
+            #FLIP
             # obs = np.flipud(self.prev_obs)
-            box_min = 7
-            box_max = 22
-            pivot_h = 12
-            pivot_w = 24
-            w1 = np.random.randint(box_min, box_max)
-            h1 = np.random.randint(box_min, box_max)
 
-            self.prev_obs[pivot_h+h1:pivot_h+h1+h1, 
-                    pivot_w+w1:pivot_w+w1+w1, :] = 0
-            obs = self.prev_obs
+            #CUTOUT
+            # box_min = 7
+            # box_max = 22
+            # pivot_h = 12
+            # pivot_w = 24
+            # w1 = np.random.randint(box_min, box_max)
+            # h1 = np.random.randint(box_min, box_max)
+
+            # self.prev_obs[pivot_h+h1:pivot_h+h1+h1, 
+            #         pivot_w+w1:pivot_w+w1+w1, :] = 0
+
+
+            #RAND CROP
+
+            self.prev_obs = np.transpose(self.prev_obs, (1, 0, 2))
+
+            crop_size = 64
+            crop_max = 75 - crop_size
+            w1 = np.random.randint(0, crop_max)
+            h1 = np.random.randint(0, crop_max)
+
+            # creates all sliding windows combinations of size (output_size)
+            windows = view_as_windows(
+                self.prev_obs, (crop_size, crop_size, 1))[0,:,:, 0]
+            # selects a random window for each batch element
+            cropped_img = windows[w1, h1]
+            cropped_img = np.swapaxes(cropped_img,0,2)
+
+            obs = cropped_img
             reward = self.prev_reward
             done = self.prev_done
             info = self.prev_info
