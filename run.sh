352b5d717425c4e795cc66301c676bd06f04dbc5
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-27 18:17:21 -0400

    s

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ set -e
 #########################################
 
 #will: change this to whatever experiment config
-export EXPERIMENT_DEFAULT="experiments/tune-marwil.yaml"
+export EXPERIMENT_DEFAULT="experiments/impala-baseline.yaml"
 export EXPERIMENT=${EXPERIMENT:-$EXPERIMENT_DEFAULT}
 
 # export CHECKPOINT=~/ray_results/procgen-ppo/PPO_stacked_procgen_env_0_2020-08-18_22-49-2719c_37sl/checkpoint_7/checkpoint-7
@@ -21,9 +21,13 @@ if [[ -z $AICROWD_IS_GRADING ]]; then
 
 #will: https://discourse.aicrowd.com/t/aws-instance-setup/3380
   export OUTPUTS_DIR=./outputs
-  export RAY_MEMORY_LIMIT=60129542144
-  export RAY_CPUS=8
-  export RAY_STORE_MEMORY=30000000000
+  export RAY_MEMORY_LIMIT=480000000000
+  export RAY_CPUS=32
+  export RAY_STORE_MEMORY=240000000000
+
+  # export RAY_MEMORY_LIMIT=60129542144
+  # export RAY_CPUS=8
+  # export RAY_STORE_MEMORY=30000000000
 
   # export OUTPUTS_DIR=./outputs
   # export RAY_MEMORY_LIMIT=6012954214
