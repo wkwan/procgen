c4d6d490d00acf484f4a56ec95eeb6350508eded
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-26 20:18:09 -0400

    s

--- train.py
+++ train.py
@@ -149,8 +149,8 @@ def run(args, parser):
     if args.config_file:
         with open(args.config_file) as f:
             experiments = yaml.safe_load(f)
-            
-            EXPERIMENT_NAME = 'tune-sac-baseline-10samples-10ksteps'
+
+            EXPERIMENT_NAME = 'tune-ppo-10samples-10ksteps'
             experiments[EXPERIMENT_NAME]['config']['adam_epsilon'] = ray.tune.uniform(1e-8, 1e-6)
             experiments[EXPERIMENT_NAME]['num_samples'] = 10
             # experiments['tune-impala-baseline-rand']['config']['num_sgd_iter'] = ray.tune.randint(3, 30)
