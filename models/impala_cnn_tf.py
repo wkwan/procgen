02129fc1fe2507e0a2fd5e28e517489424cf6534
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-28 13:32:52 -0400

    s

--- models/impala_cnn_tf.py
+++ models/impala_cnn_tf.py
@@ -78,7 +78,7 @@ class ImpalaCNN(TFModelV2):
     def forward(self, input_dict, state, seq_lens):
         # explicit cast to float32 needed in eager
         obs = tf.cast(input_dict["obs"], tf.float32)
-        print("forward pass yo")
+        print(obs)
         logits, self._value = self.base_model(obs)
         return logits, state
 
