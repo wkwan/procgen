eab275f3dfd0da057ce6b4640b5e2dc62f2c79c2
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-29 14:22:47 -0400

    s

--- envs/framestack_frameskip.py
+++ envs/framestack_frameskip.py
@@ -6,16 +6,16 @@ from envs.procgen_env_wrapper import ProcgenEnvWrapper
 import numpy as np
 from gym import ActionWrapper
 
-class FrameSkip(ActionWrapper, FrameStack):
-    def step(self, action):
-        self.env.step(self.action(action))
-        return self.env.step(self.action(action))
+# class FrameSkip(ActionWrapper, FrameStack):
+#     def step(self, action):
+#         self.env.step(self.action(action))
+#         return self.env.step(self.action(action))
 
-    def action(self, action):
-        return action
+#     def action(self, action):
+#         return action
 
 # Register Env in Ray
 registry.register_env(
     "framestack_frameskip_procgen_env",  # This should be different from procgen_env_wrapper
-    lambda config: FrameSkip(ProcgenEnvWrapper(config), 2),
+    lambda config: FrameStack(ProcgenEnvWrapper(config), 2),
 )
