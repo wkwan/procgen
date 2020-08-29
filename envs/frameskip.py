a5b5700fabb69474a5685f785f71dd5098626c36
William Kwan <wk@Williams-MacBook-Pro.local>
2020-08-29 13:01:43 -0400

    s

---
+++ envs/frameskip.py
@@ -0,0 +1,17 @@
+from ray.tune import registry
+
+from envs.procgen_env_wrapper import ProcgenEnvWrapper
+
+import numpy as np
+from gym import ActionWrapper
+
+class FrameSkip(ActionWrapper):
+    def step(self, action):
+        self.env.step(self.action(action))
+        return self.env.step(self.action(action))
+
+# Register Env in Ray
+registry.register_env(
+    "frameskip_procgen_env",  # This should be different from procgen_env_wrapper
+    lambda config: FrameSkip(ProcgenEnvWrapper(config)),
+)
--- test_model.py
+++ test_model.py
@@ -1,8 +1,82 @@
-from ray.rllib.models.tf.tf_modelv2 import TFModelV2
-from ray.rllib.utils.framework import try_import_tf
-from ray.rllib.models import ModelCatalog
+import tensorflow as tf
 
-tf = try_import_tf()
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+import numpy
+import sys
+numpy.set_printoptions(threshold=sys.maxsize)
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+#                'dog', 'frog', 'horse', 'ship', 'truck']
+
+print(train_images.shape)
+
+
+
+
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     # The CIFAR labels happen to be arrays, 
+#     # which is why you need the extra index
+#     plt.xlabel(class_names[train_labels[i][0]])
+# plt.show()
+
+# model = models.Sequential()
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+# model.add(layers.MaxPooling2D((2, 2)))
+
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(10))
+
+# model.summary()
+
+# model.compile(optimizer='adam',
+#               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#               metrics=['accuracy'])
+
+
+
+
+# history = model.fit(train_images, train_labels, epochs=1, 
+#                     validation_data=(test_images, test_labels))
+
+# get_3rd_layer_output = tf.keras.backend.function([model.layers[0].input],
+#                                   [model.layers[3].output])
+# layer_output = get_3rd_layer_output([train_images, train_labels])[0]
+# print("3rd layer output")
+# layer_output_np = tf.keras.backend.eval(layer_output)
+# print(layer_output_np)
+
+# plt.plot(history.history['accuracy'], label='accuracy')
+# plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
+# plt.ylim([0.5, 1])
+# plt.legend(loc='lower right')
+
+# test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+# print(test_acc)
+
+
+
+
 
 def conv_layer(depth, name):
     layer = tf.keras.layers.Conv2D( 
@@ -36,7 +110,7 @@ def conv_sequence(x, depth, prefix):
     return x
 
 
-class ImpalaCNN(TFModelV2):
+class ImpalaCNN():
     """
     Network from IMPALA paper implemented in ModelV2 API.
 
@@ -55,8 +129,6 @@ class ImpalaCNN(TFModelV2):
 
         for i, depth in enumerate(depths):
             x = conv_sequence(x, depth, prefix=f"seq{i}")
-
-        # x = tf.keras.backend.print_tensor(x, message='last conv')
 
         x = tf.keras.layers.Flatten()(x)
         
@@ -93,5 +165,3 @@ class ImpalaCNN(TFModelV2):
         return tf.reshape(self._value, [-1])
 
 
-# Register model in ModelCatalog
-ModelCatalog.register_custom_model("impala_cnn_tf", ImpalaCNN)
