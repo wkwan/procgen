ebc65dbad217473905562a9cec453a27c268d6b9
William Kwan <wk@Williams-MacBook-Pro.local>
2020-09-02 13:24:54 -0400

    s

--- algorithms/ucb_drac_agent/trainer.py
+++ algorithms/ucb_drac_agent/trainer.py
@@ -17,9 +17,9 @@ DEFAULT_CONFIG = with_common_config({
     # with a value function, see https://arxiv.org/pdf/1506.02438.pdf.
     "use_gae": True,
     # The GAE(lambda) parameter.
-    "lambda": 1.0,
+    "lambda": 0.95,
     # Initial coefficient for KL divergence.
-    "kl_coeff": 0.2,
+    "kl_coeff": 0.0,
     # Size of batches collected from each worker.
     "rollout_fragment_length": 256,
     # Number of timesteps collected for each SGD round. This defines the size
@@ -34,26 +34,26 @@ DEFAULT_CONFIG = with_common_config({
     # execute per train batch).
     "num_sgd_iter": 3,
     # Stepsize of SGD.
-    "lr": 5e-5,
+    "lr": 5.0e-4,
     # Learning rate schedule.
     "lr_schedule": None,
     # Share layers for value function. If you set this to True, it's important
     # to tune vf_loss_coeff.
-    "vf_share_layers": False,
+    "vf_share_layers": True,
     # Coefficient of the value function loss. IMPORTANT: you must tune this if
     # you set vf_share_layers: True.
-    "vf_loss_coeff": 1.0,
+    "vf_loss_coeff": 0.5,
     # Coefficient of the entropy regularizer.
-    "entropy_coeff": 0.0,
+    "entropy_coeff": 0.01,
     # Decay schedule for the entropy regularizer.
-    "entropy_coeff_schedule": None,
+    "entropy_coeff_schedule": 0.01,
     # PPO clip parameter.
-    "clip_param": 0.3,
+    "clip_param": 0.2,
     # Clip param for the value function. Note that this is sensitive to the
     # scale of the rewards. If your expected V is large, increase this.
-    "vf_clip_param": 10.0,
+    "vf_clip_param": 0.2,
     # If specified, clip the global norm of gradients by this amount.
-    "grad_clip": None,
+    "grad_clip": 0.5,
     # Target value for KL divergence.
     "kl_target": 0.01,
     # Whether to rollout "complete_episodes" or "truncate_episodes".
@@ -68,7 +68,7 @@ DEFAULT_CONFIG = with_common_config({
     # Set this to True for debugging on non-GPU machines (set `num_gpus` > 0).
     "_fake_gpus": False,
     # Use PyTorch as framework?
-    "use_pytorch": False
+    "use_pytorch": True
 })
 # __sphinx_doc_end__
 # yapf: enable
