ed87a84429d0f4282ae44cdc0bf3ea445c356304
William Kwan <wk@Williams-MacBook-Pro.local>
2020-09-03 13:48:48 -0400

    s

--- algorithms/ucb_drac_agent/torch_policy_template.py
+++ algorithms/ucb_drac_agent/torch_policy_template.py
@@ -1,5 +1,5 @@
 from ray.rllib.policy.policy import Policy
-.torch_policy import TorchPolicy
+from .torch_policy import TorchPolicy
 from ray.rllib.models.catalog import ModelCatalog
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
 from ray.rllib.utils import add_mixins
