705858d7f94585d4b32cf187e62eb4cf26a6c183
William Kwan <wk@Williams-MacBook-Pro.local>
2020-09-06 20:57:26 -0400

    s

--- algorithms/ucb_drac_agent/ucb_ppo_policy.py
+++ algorithms/ucb_drac_agent/ucb_ppo_policy.py
@@ -572,11 +572,11 @@ def Identity(x):
     return x
 
 aug_to_func = {    
-        # 'crop': Crop, #works
-        # 'random-conv': RandomConv,
-        # 'grayscale': Grayscale, #works
-        # 'flip': Flip, #works
-        # 'rotate': Rotate, #works
+        'crop': Crop, #works
+        'random-conv': RandomConv,
+        'grayscale': Grayscale, #works
+        'flip': Flip, #works
+        'rotate': Rotate, #works
         'cutout': Cutout, #works
         'cutout-color': CutoutColor, #works
         'color-jitter': ColorJitter #works
