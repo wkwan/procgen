b4a16ad73a8ce48c94be4fb82a8332993e663173
William Kwan <wk@Williams-MacBook-Pro.local>
2020-09-07 14:05:56 -0400

    s

--- algorithms/ucb_drac_agent/ucb_ppo_policy.py
+++ algorithms/ucb_drac_agent/ucb_ppo_policy.py
@@ -245,7 +245,7 @@ class Rotate(object):
             tot_imgs = np.concatenate((tot_imgs, rot_imgs), 0)
         images = torch.tensor(tot_imgs[self.random_inds])
         print("to write shape",tot_imgs[self.random_inds][0].shape)
-        imageio.imwrite('/home/ubuntu/procgen-competition/rotate.png', tot_imgs[self.random_inds][0].cpu().detach().numpy())
+        imageio.imwrite('/home/ubuntu/procgen-competition/rotate.png', tot_imgs[self.random_inds][0])
 
         return images
     
