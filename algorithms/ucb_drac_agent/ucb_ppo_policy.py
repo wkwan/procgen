4ea3130edf24be1c44315d1f05f901b48b0b0564
William Kwan <wk@Williams-MacBook-Pro.local>
2020-09-09 16:09:53 -0400

    s

--- algorithms/ucb_drac_agent/ucb_ppo_policy.py
+++ algorithms/ucb_drac_agent/ucb_ppo_policy.py
@@ -756,7 +756,7 @@ def ppo_surrogate_loss(policy, model, dist_class, train_batch, update_train_batc
 
     # print("sample aug actions log p", aug_train_batch[SampleBatch.ACTION_LOGP])
 
-    aug_actions_sample = action_loss_aug.sample()
+    aug_actions_sample = aug_action_dist.sample()
     print("aug action sample", aug_actions_sample)
     action_loss_aug = - torch.mean(aug_actions_sample)
     print("action_loss_aug", action_loss_aug)
