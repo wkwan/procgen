453b4c445363fe1d91eb0e53583ab97a060e7436
William Kwan <wk@Williams-MacBook-Pro.local>
2020-09-09 16:45:15 -0400

    s

--- algorithms/ucb_drac_agent/ucb_ppo_policy.py
+++ algorithms/ucb_drac_agent/ucb_ppo_policy.py
@@ -588,13 +588,13 @@ def Identity(x):
     return x
 
 aug_to_func = {    
-        'crop': Crop, #works
-        'random-conv': RandomConv, #works
-        'cutout-color': CutoutColor, #works
-        'color-jitter': ColorJitter, #works
-        'rotate': Rotate, #works but maybe not doing the intended rotation? shouldn't make a diff tho
-        'flip': Flip, #works
-        'cutout': Cutout, #works
+        # 'crop': Crop, #works
+        # 'random-conv': RandomConv, #works
+        # 'cutout-color': CutoutColor, #works
+        # 'color-jitter': ColorJitter, #works
+        # 'rotate': Rotate, #works but maybe not doing the intended rotation? shouldn't make a diff tho
+        # 'flip': Flip, #works
+        # 'cutout': Cutout, #works
         'grayscale': Grayscale, #works
 
 }
@@ -743,7 +743,7 @@ def ppo_surrogate_loss(policy, model, dist_class, train_batch, update_train_batc
     # return policy.loss_obj.loss
 
     # print("sample actions log p", train_batch[SampleBatch.ACTION_LOGP])
-    aug_train_batch = train_batch
+    # aug_train_batch = train_batch
 
     current_aug_func = aug_list[ucb_aug_id]
 
@@ -753,14 +753,14 @@ def ppo_surrogate_loss(policy, model, dist_class, train_batch, update_train_batc
     # aug_train_batch[SampleBatch.PREV_ACTIONS] = aug_train_batch[SampleBatch.ACTIONS]
     # aug_train_batch[SampleBatch.PREV_REWARDS] = aug_train_batch[SampleBatch.PREV_REWARDS]
 
-    aug_logits, aug_state = model.forward({"obs": current_aug_func.do_augmentation(aug_train_batch[SampleBatch.CUR_OBS]).cuda()}, None, None)
+    aug_logits, aug_state = model.forward({"obs": current_aug_func.do_augmentation(train_batch[SampleBatch.CUR_OBS]).cuda()}, None, None)
     # aug_action_dist = dist_class(aug_logits, model)
     # aug_actions_sample = aug_action_dist.sample()
     # aug_train_batch[SampleBatch.ACTION_DIST_INPUTS] = aug_actions_sample
     # print("sample aug actions log p", aug_train_batch[SampleBatch.ACTION_LOGP])
 
     # print("aug action sample", aug_actions_sample)
-    action_loss_aug = torch.mean(aug_logits)
+    action_loss_aug = - torch.mean(aug_logits)
     print("action_loss_aug", action_loss_aug)
     value_loss_aug = 0.5 * (prev_value_function_result - model.value_function()).pow(2).mean()
     print("value loss aug", value_loss_aug)
