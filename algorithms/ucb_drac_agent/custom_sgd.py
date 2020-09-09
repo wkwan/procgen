e6111384c56d11bfec417e5e60987742a5d702c5
William Kwan <wk@Williams-MacBook-Pro.local>
2020-09-08 20:39:09 -0400

    s

--- algorithms/ucb_drac_agent/custom_sgd.py
+++ algorithms/ucb_drac_agent/custom_sgd.py
@@ -117,7 +117,7 @@ def do_minibatch_sgd(samples, policies, local_worker, num_sgd_iter,
                     }, minibatch.count)))[policy_id]
                 if batch_fetches is None:
                     print("skip the k v in batch fetches")
-                    continue
+                    return {}
                 for k, v in batch_fetches.get(LEARNER_STATS_KEY, {}).items():
                     iter_extra_fetches[k].append(v)
             logger.debug("{} {}".format(i, averaged(iter_extra_fetches)))
