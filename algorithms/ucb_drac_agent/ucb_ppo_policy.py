7806b9567c529e036fc90cb3dbd07859905d466c
William Kwan <wkwan94@gmail.com>
2020-09-29 23:27:38 -0400

    s

--- algorithms/ucb_drac_agent/ucb_ppo_policy.py
+++ algorithms/ucb_drac_agent/ucb_ppo_policy.py
@@ -271,11 +271,8 @@ class Crop(object):
         self.batch_size = batch_size 
 
     def do_augmentation(self, x):
-
-        print(x.device)
         x = x.clone()
         x = x.permute(0,3,1,2).to(dtype=torch.float32)
-        x = x.permute(0,3,1,2)
         aug_trans = nn.Sequential(nn.ReplicationPad2d(12),
                             kornia.augmentation.RandomCrop((64, 64)))
         x = aug_trans(x)
