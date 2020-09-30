25fc5ea455283bf4652812a8cd8b96c0657807b4
William Kwan <wkwan94@gmail.com>
2020-09-30 00:05:43 -0400

    s

--- algorithms/ucb_drac_agent/trainer.py
+++ algorithms/ucb_drac_agent/trainer.py
@@ -32,7 +32,7 @@ DEFAULT_CONFIG = with_common_config({
     "rollout_fragment_length": 256,
     # Number of timesteps collected for each SGD round. This defines the size
     # of each SGD epoch.
-    "train_batch_size": 16384,
+    "train_batch_size": 32768,
     # Total SGD batch size across all devices for SGD. This defines the
     # minibatch size within each epoch.
     "sgd_minibatch_size": 256,
