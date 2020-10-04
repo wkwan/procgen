6b2953c8c602174cf3a2989834132a2c8f5159fb
William Kwan <wkwan94@gmail.com>
2020-10-04 00:19:38 -0400

    s

--- envs/crop.py
+++ envs/crop.py
@@ -38,7 +38,7 @@ class Crop(ObservationWrapper):
 
             #RAND CROP
             print("type", type(self.prev_obs))
-            self.prev_obs = torch.from_numpy(self.prev_obs.copy()).float()
+            self.prev_obs = torch.from_numpy(self.prev_obs.copy()).float(dtype=torch.float32)
             print("shape", self.prev_obs.shape)
             self.prev_obs = self.prev_obs.permute(2,0,1)
             aug_trans = nn.Sequential(nn.ReplicationPad2d((12,12)),
