f5aeb4af028dd1d1ab9b0d7dbb0ea73d16125d89
William Kwan <wkwan94@gmail.com>
2020-10-04 00:10:43 -0400

    s

--- envs/crop.py
+++ envs/crop.py
@@ -39,11 +39,11 @@ class Crop(ObservationWrapper):
             #RAND CROP
             self.prev_obs = torch.from_numpy(self.prev_obs.copy()).float()
             print("shape", self.prev_obs.shape)
-            self.prev_obs = self.prev_obs.permute(0,3,1,2)
+            self.prev_obs = self.prev_obs.permute(2,0,1)
             aug_trans = nn.Sequential(nn.ReplicationPad2d(12),
                                 kornia.augmentation.RandomCrop((64, 64)))
             self.prev_obs = aug_trans(self.prev_obs)
-            self.prev_obs = self.prev_obs.permute(0,2,3,1)
+            self.prev_obs = self.prev_obs.permute(1,2,0)
             self.prev_obs = self.prev_obs.detach().numpy
 
             obs = self.prev_obs
