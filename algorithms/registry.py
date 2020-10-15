85bf623d5162e338f1b2a6f0e887c1cd97e4a82c
William Kwan <wkwan94@gmail.com>
2020-10-15 12:43:36 -0400

    s

--- algorithms/registry.py
+++ algorithms/registry.py
@@ -21,8 +21,12 @@ def _import_random_policy():
     from .random_policy.trainer import RandomPolicyTrainer
     return RandomPolicyTrainer
 
+def _ppo_openai():
+    from .ppo_torch.ppo_torch import PPOTrainer
+    return PPOTrainer
 
 CUSTOM_ALGORITHMS = {
     "custom/CustomRandomAgent": _import_custom_random_agent,
-    "RandomPolicy": _import_random_policy
+    "RandomPolicy": _import_random_policy,
+    "PPOOpenAI": _ppo_openai
 }
