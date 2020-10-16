2e82cf149445aa96ebeed4901fe113dff0815e58
William Kwan <wkwan94@gmail.com>
2020-10-15 23:13:18 -0400

    s

--- models/impala_ppg.py
+++ models/impala_ppg.py
@@ -384,11 +384,10 @@ class ImpalaCNN(TorchModelV2, nn.Module):
 
         # b = x.shape[:-3]
         # x = x.reshape(b, x.shape[-3:])
-        # x = transpose(x, "bhwc", "bchw")
-        x = x.permute(0, 3, 1, 2)
+        x = transpose(x, "bhwc", "bchw")
+        # x = x.permute(0, 3, 1, 2)
         x = sequential(self.stacks, input_dict, state, seq_lens, diag_name=self.name)
         # x = x.reshape(b, x.shape[1:])
-        x = x.permute(0, 2, 3, 1)
 
         x = flatten_image(x)
         x = th.relu(x)
