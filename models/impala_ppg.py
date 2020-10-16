47c8dbec0758bdf667f5659189109e3270dda4e2
William Kwan <wkwan94@gmail.com>
2020-10-15 21:53:02 -0400

    s

--- models/impala_ppg.py
+++ models/impala_ppg.py
@@ -4,9 +4,220 @@ import torch as th
 from torch import nn
 from torch.nn import functional as F
 
-from . import torch_util as tu
 from gym3.types import Real, TensorType
 REAL = Real()
+
+
+import collections
+import functools
+import itertools
+import math
+import os
+import platform
+from contextlib import contextmanager
+import re
+
+import numpy as np
+import torch.distributed as dist
+import torch.distributions as dis
+import socket
+import time
+import random
+import multiprocessing as mp
+
+def have_cuda():
+    return (
+        th.has_cuda and th.cuda.is_available() and not os.getenv("RCALL_NUM_GPU") == "0"
+    )
+
+def default_device_type():
+    return "cuda" if have_cuda() else "cpu"
+
+def ftensor(*args, **kwargs):
+    return th.tensor(*args, **kwargs, device=dev(), dtype=th.float32)
+
+
+def ltensor(*args, **kwargs):
+    return th.tensor(*args, **kwargs, device=dev(), dtype=th.int64)
+
+
+def zeros(*args, **kwargs):
+    return th.zeros(*args, **kwargs, device=dev())
+
+
+def ones(*args, **kwargs):
+    return th.ones(*args, **kwargs, device=dev())
+
+
+def arange(*args, **kwargs):
+    return th.arange(*args, **kwargs, device=dev())
+
+
+def np2th(nparr):
+    dtype = th.float32 if nparr.dtype == np.float64 else None
+    return th.from_numpy(nparr).to(device=dev(), dtype=dtype)
+
+
+def th2np(tharr):
+    return tharr.cpu().numpy()
+
+
+def NormedLinear(*args, scale=1.0, dtype=th.float32, **kwargs):
+    """
+    nn.Linear but with normalized fan-in init
+    """
+    dtype = parse_dtype(dtype)
+    if dtype == th.float32:
+        out = nn.Linear(*args, **kwargs)
+    elif dtype == th.float16:
+        out = LinearF16(*args, **kwargs)
+    else:
+        raise ValueError(dtype)
+    out.weight.data *= scale / out.weight.norm(dim=1, p=2, keepdim=True)
+    if kwargs.get("bias", True):
+        out.bias.data *= 0
+    return out
+
+def NormedConv2d(*args, scale=1, **kwargs):
+    """
+    nn.Conv2d but with normalized fan-in init
+    """
+    out = nn.Conv2d(*args, **kwargs)
+    out.weight.data *= scale / out.weight.norm(dim=(1, 2, 3), p=2, keepdim=True)
+    if kwargs.get("bias", True):
+        out.bias.data *= 0
+    return out
+
+def flatten_image(x):
+    """
+    Flattens last three dims
+    """
+    *batch_shape, h, w, c = x.shape
+    return x.reshape((*batch_shape, h * w * c))
+
+
+def sequential(layers, x, *args, diag_name=None):
+    for (i, layer) in enumerate(layers):
+        x = layer(x, *args)
+    return x
+
+def all_mean_(x, group=dist.group.WORLD):
+    dist_all_reduce(x, group=group)
+    x /= dist_get_world_size(group=group)
+    return x
+
+
+def all_mean(x):
+    return all_mean_(x.clone())
+
+
+def all_sum_(x, group=dist.group.WORLD):
+    dist_all_reduce(x, group=group)
+    return x
+
+
+def all_sum(x):
+    return all_sum_(x.clone())
+
+
+def flatten_tensors(xs, dtype=None, buf=None):
+    if buf is None:
+        buf = xs[0].new_empty(sum(x.numel() for x in xs), dtype=dtype)
+    i = 0
+    for x in xs:
+        buf[i : i + x.numel()].copy_(x.view(-1))
+        i += x.numel()
+    return buf
+
+
+def unflatten_to(newflat, xs):
+    start = 0
+    for x in xs:
+        size = x.numel()
+        end = start + size
+        x.copy_(newflat[start:end].view(x.shape))
+        start = end
+    assert start == newflat.numel()
+
+def is_distributed():
+    return dist.is_initialized()
+
+
+def dist_broadcast(*args, **kwargs):
+    if not is_distributed():
+        return
+    dist.broadcast(*args, **kwargs)
+
+
+def dist_all_reduce(*args, **kwargs):
+    if not is_distributed():
+        return
+    dist.all_reduce(*args, **kwargs)
+
+
+def dist_get_world_size(group=dist.group.WORLD):
+    if not is_distributed():
+        return 1
+    return dist.get_world_size(group=group)
+
+def sync_grads(
+    params, group=dist.group.WORLD, grad_weight=1.0, dtype=None, sync_buffer=None
+):
+    """
+    Sync gradients for the provided params across all members of the specified group
+    """
+    if not is_distributed():
+        assert group is dist.group.WORLD
+        return
+    if dist.get_world_size(group) == 1:
+        return
+    grads = [p.grad for p in params if p.grad is not None]
+    flatgrad = flatten_tensors(grads, dtype=dtype, buf=sync_buffer)
+    if grad_weight != 1.0:
+        flatgrad.mul_(grad_weight)
+    all_mean_(flatgrad, group=group)
+    unflatten_to(flatgrad, grads)
+
+def _numpy_allmean(comm, x):
+    out = np.zeros_like(x)
+    comm.Allreduce(x, out)
+    out /= comm.size
+    return out
+
+def parse_dtype(x):
+    if isinstance(x, th.dtype):
+        return x
+    elif isinstance(x, str):
+        if x == "float32" or x == "float":
+            return th.float32
+        elif x == "float64" or x == "double":
+            return th.float64
+        elif x == "float16" or x == "half":
+            return th.float16
+        elif x == "uint8":
+            return th.uint8
+        elif x == "int8":
+            return th.int8
+        elif x == "int16" or x == "short":
+            return th.int16
+        elif x == "int32" or x == "int":
+            return th.int32
+        elif x == "int64" or x == "long":
+            return th.int64
+        elif x == "bool":
+            return th.bool
+        else:
+            raise ValueError(f"cannot parse {x} as a dtype")
+    else:
+        raise TypeError(f"cannot parse {type(x)} as dtype")
+
+def sum_nonbatch(x, nbatchdim=2):
+    return x.sum(dim=tuple(range(nbatchdim, x.dim()))) if x.dim() > nbatchdim else x
+
+def _process_modelpath(path, stage_index):
+    # if we have a pipelined model, the user should specify a path with stage-0 in the filename
+    # replace it with the correct stage
+    return path.replace("-stage-0", f"-stage-{stage_index}")
 
 class Encoder(nn.Module):
     """
