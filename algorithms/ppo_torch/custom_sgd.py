2ad2c69e3ca3be182ea266064543c445d27ed863
William Kwan <wkwan94@gmail.com>
2020-10-24 18:29:09 -0400

    s

--- algorithms/ppo_torch/custom_sgd.py
+++ algorithms/ppo_torch/custom_sgd.py
@@ -204,8 +204,8 @@ def do_minibatch_sgd(samples, policies, local_worker, num_sgd_iter,
                     pd = dist_class(logits, model)
                     print("newpd", pd)
                     name2loss = {}
-                    name2loss["pol_distance"] = td.kl_divergence(mb["oldpd"], pd).mean()
-                    print("pol dist", name2loss["pol_distance"])
+                    # name2loss["pol_distance"] = td.kl_divergence(mb["oldpd"], pd).mean()
+                    # print("pol dist", name2loss["pol_distance"])
                     # name2loss.update(compute_aux_loss(aux, mb))
             seg_buf.clear()
         fetches[policy_id] = averaged(iter_extra_fetches)
