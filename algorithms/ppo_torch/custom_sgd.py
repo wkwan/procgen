b1c99bda3ae2e201867b4309b44b2853995cc150
William Kwan <wkwan94@gmail.com>
2020-10-24 14:22:06 -0400

    s

--- algorithms/ppo_torch/custom_sgd.py
+++ algorithms/ppo_torch/custom_sgd.py
@@ -175,6 +175,7 @@ def do_minibatch_sgd(samples, policies, local_worker, num_sgd_iter,
             #compute presleep outputs for replay buffer (what does this mean?)
             for seg in seg_buf:
                 seg["obs"] = th.from_numpy(seg["obs"]).to(th.cuda.current_device())
+                print("segs oldpd", seg['oldpd'])
                 # logits, state = model.forward(seg, None, None)
 
             #train on replay buffer
