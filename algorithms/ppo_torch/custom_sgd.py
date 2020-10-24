f0c0cd00161304789d32ebb1279393bf9d0e0b80
William Kwan <wkwan94@gmail.com>
2020-10-24 15:14:38 -0400

    s

--- algorithms/ppo_torch/custom_sgd.py
+++ algorithms/ppo_torch/custom_sgd.py
@@ -192,8 +192,8 @@ def do_minibatch_sgd(samples, policies, local_worker, num_sgd_iter,
             for i in range(9):
                 for mb in make_minibatches(seg_buf, 4):
                     mb = tree_map(lambda x: x.to(tu.dev()), mb)
-                    # print("a mb")
-                    print("oldpd", mb['oldpd'])
+                    print("a mb")
+                    # print("oldpd", mb['oldpd'])
                     # logits, state = model.forward(mb, None, None)
                     # pd = dist_class(logits, model)
 
