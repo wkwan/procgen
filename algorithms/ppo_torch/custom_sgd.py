f9d8746485185e625b8b6b0f1aa74e436a0b9ab6
William Kwan <wkwan94@gmail.com>
2020-10-25 22:46:28 -0400

    s

--- algorithms/ppo_torch/custom_sgd.py
+++ algorithms/ppo_torch/custom_sgd.py
@@ -192,7 +192,7 @@ def do_minibatch_sgd(samples, policies, local_worker, num_sgd_iter,
             for seg in seg_buf:
                 seg["obs"] = th.from_numpy(seg["obs"]).to(th.cuda.current_device())
                 logits, state = tu.minibatched_call(forward, 4, seg=seg)
-                print("presleep logits", logits)
+                print("presleep logits", logits.shape, logits,)
                 # print("logits splice", logits[2:])
                 # seg["oldpd"] = dist_class(logits)
                 # print("seg presleep oldpd", logits.shape, logits)
