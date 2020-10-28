1e9099a254fd0e8ef47682faedce12f31a1c7d12
William Kwan <wkwan94@gmail.com>
2020-10-28 16:42:13 -0400

    s

--- algorithms/ppo_torch/custom_torch_policy_template.py
+++ algorithms/ppo_torch/custom_torch_policy_template.py
@@ -182,12 +182,9 @@ def build_torch_policy(name,
         @override(TorchPolicy)
         def extra_action_out(self, input_dict, state_batches, model,
                              action_dist):
-            print("call this extra action out with grad")
             
             # with torch.no_grad():
             if extra_action_out_fn:
-
-                # print("inside extra action out")
 
                 stats_dict = extra_action_out_fn(
                     self, input_dict, state_batches, model, action_dist)
