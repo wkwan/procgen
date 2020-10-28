697f430b2a9b15421b56fd3857bff795bda77d7a
William Kwan <wkwan94@gmail.com>
2020-10-28 16:23:15 -0400

    s

--- algorithms/ppo_torch/ppo_torch_policy.py
+++ algorithms/ppo_torch/ppo_torch_policy.py
@@ -255,7 +255,7 @@ class ValueNetworkMixin:
                     "is_training": False,
                 }, [self._convert_to_tensor(s) for s in state],
                                           self._convert_to_tensor([1]))
-                # print("value mixin", self.model.value_function()[0])
+                print("value mixin", self.model.value_function()[0])
                 return self.model.value_function()[0]
 
         else:
