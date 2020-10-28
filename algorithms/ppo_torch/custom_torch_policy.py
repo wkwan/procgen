3cdcd79726302165f869104afe0e02869da6ab40
William Kwan <wkwan94@gmail.com>
2020-10-28 14:13:24 -0400

    s

--- algorithms/ppo_torch/custom_torch_policy.py
+++ algorithms/ppo_torch/custom_torch_policy.py
@@ -353,7 +353,7 @@ class TorchPolicy(Policy):
         for i, opt in enumerate(self._optimizers):
             opt.zero_grad()
             pi_loss = loss_out[i]
-            self.backprop(grad_info, opt, pi_loss, True)
+            self.backprop(grad_info, opt, pi_loss, False)
             tu.sync_grads(list(self.model.parameters()))
             opt.step()
 
