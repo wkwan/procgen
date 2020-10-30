7ca2f40c67c8f4a40741ce1b6bb5144eb12d36e7
William Kwan <wkwan94@gmail.com>
2020-10-30 09:22:58 -0400

    s

--- models/impala_cnn_torch.py
+++ models/impala_cnn_torch.py
@@ -151,14 +151,8 @@ class ImpalaCNN(TorchModelV2, nn.Module):
         x = self.hidden_fc(x)
         x = nn.functional.relu(x)
         logits = self.logits_fc(x)
-        # print("x before detach", x)
-        x_detached = x.detach() #detach during policy phase only
-        # print("x after detach", x)
-        value = self.value_fc(x_detached)
-        # print("value before squeeze", value)
+        value = self.value_fc(x.detach()) #detach during policy phase only
         self._value = value.squeeze(1)
-        # print("value after squeeze", self._value)
-        # print("logits", logits)
         return logits, state
 
     def forward_aux(self, input_dict):
