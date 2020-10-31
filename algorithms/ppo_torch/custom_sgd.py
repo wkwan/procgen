ed7fbd7783cc67db2a9a1d338a89b3421e2f812b
William Kwan <wkwan94@gmail.com>
2020-10-31 15:04:22 -0400

    s

--- algorithms/ppo_torch/custom_sgd.py
+++ algorithms/ppo_torch/custom_sgd.py
@@ -185,7 +185,7 @@ def do_minibatch_sgd(samples, policies, local_worker, num_sgd_iter,
                     pol_distance = oldpd.kl(pd).mean()
 
                     vpredtrue = model.value_function()
-
+                    print("aux phase", vpredtrue, mb[SampleBatch.VF_PREDS])
                     vtarg = th.from_numpy(mb[Postprocessing.VALUE_TARGETS]).to(th.cuda.current_device())
                     vf_aux = 0.5 * th.mean(th.pow(vpredaux - vtarg, 2.0))
                     vf_true = 0.5 * th.mean(th.pow(vpredtrue - vtarg, 2.0))
