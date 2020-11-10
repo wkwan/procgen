3c363d4af6b3fe8e827105040967b8b025d4deb5
William Kwan <wkwan94@gmail.com>
2020-11-10 09:27:47 -0500

    delete prints

---
+++ algorithms/ppg_torch/custom_postprocessing.py
@@ -0,0 +1,251 @@
+import numpy as np
+import scipy.signal
+from ray.rllib.policy.sample_batch import SampleBatch
+from ray.rllib.utils.annotations import DeveloperAPI
+
+import numpy as np
+
+import torch as th
+import torch.distributed as dist
+from torch import nn
+
+import os
+
+def flatten_tensors(xs, dtype=None, buf=None):
+    if buf is None:
+        buf = xs[0].new_empty(sum(x.numel() for x in xs), dtype=dtype)
+    i = 0
+    for x in xs:
+        buf[i : i + x.numel()].copy_(x.view(-1))
+        i += x.numel()
+    return buf
+
+def have_cuda():
+    return (
+        th.has_cuda and th.cuda.is_available() and not os.getenv("RCALL_NUM_GPU") == "0"
+    )
+
+def is_distributed():
+    return dist.is_initialized()
+
+def dist_all_reduce(*args, **kwargs):
+    if not is_distributed():
+        return
+    dist.all_reduce(*args, **kwargs)
+
+
+def dist_get_world_size(group=dist.group.WORLD):
+    if not is_distributed():
+        return 1
+    return dist.get_world_size(group=group)
+
+
+def default_device_type():
+    return "cuda" if have_cuda() else "cpu"
+
+DEFAULT_DEVICE = th.device(type=default_device_type())
+
+def all_mean_(x, group=dist.group.WORLD):
+    dist_all_reduce(x, group=group)
+    x /= dist_get_world_size(group=group)
+    return x
+
+
+def all_mean(x):
+    return all_mean_(x.clone())
+
+def unflatten_to(newflat, xs):
+    start = 0
+    for x in xs:
+        size = x.numel()
+        end = start + size
+        x.copy_(newflat[start:end].view(x.shape))
+        start = end
+    assert start == newflat.numel()
+
+
+class RunningMeanStd(nn.Module):
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(
+        self,
+        epsilon: "initial count (with mean=0 ,var=1)" = 1e-4,
+        shape: "unbatched shape of data" = (),
+        distributed: "whether to allreduce stats" = True,
+    ):
+        super().__init__()
+        self.register_buffer("mean", th.zeros(shape))
+        self.register_buffer("var", th.ones(shape))
+        self.register_buffer("count", th.tensor(epsilon))
+        self.distributed = distributed and is_distributed()
+
+    def update(self, x):
+        batch_mean = x.mean(dim=0)
+        batch_var = x.var(dim=0, unbiased=False)
+        batch_count = th.tensor([x.shape[0]], device=x.device, dtype=th.float32)
+        if self.distributed:
+            # flatten+unflatten so we just need one allreduce
+            flat = flatten_tensors([batch_mean, batch_var, batch_count])
+            flat = flat.to(device=DEFAULT_DEVICE)  # Otherwise all_mean_ will fail
+            all_mean_(flat)
+            unflatten_to(flat, [batch_mean, batch_var, batch_count])
+            batch_count *= dist.get_world_size()
+        self.update_from_moments(batch_mean, batch_var, batch_count[0])
+
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        # pylint: disable=attribute-defined-outside-init
+        self.mean, self.var, self.count = update_mean_var_count_from_moments(
+            self.mean, self.var, self.count, batch_mean, batch_var, batch_count
+        )
+
+
+def update_mean_var_count_from_moments(
+    mean, var, count, batch_mean, batch_var, batch_count
+):
+    delta = batch_mean - mean
+    tot_count = count + batch_count
+
+    new_mean = mean + delta * batch_count / tot_count
+    m_a = var * count
+    m_b = batch_var * batch_count
+    M2 = m_a + m_b + delta ** 2 * count * batch_count / tot_count
+    new_var = M2 / tot_count
+    new_count = tot_count
+
+    return new_mean, new_var, new_count
+
+class RewardNormalizer:
+    """
+    Pseudocode can be found in https://arxiv.org/pdf/1811.02553.pdf
+    section 9.3 (which is based on our Baselines code, haha)
+    Motivation is that we'd rather normalize the returns = sum of future rewards,
+    but we haven't seen the future yet. So we assume that the time-reversed rewards
+    have similar statistics to the rewards, and normalize the time-reversed rewards.
+    """
+
+    def __init__(self, num_envs, cliprew=10.0, gamma=0.99, epsilon=1e-8, per_env=False):
+        ret_rms_shape = (num_envs,) if per_env else ()
+        self.ret_rms = RunningMeanStd(shape=ret_rms_shape)
+        self.cliprew = cliprew
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.per_env = per_env
+
+    def __call__(self, reward):
+        rets = backward_discounted_sum(
+            reward=reward, gamma=self.gamma
+        )
+        # self.ret = rets[:, -1]
+        self.ret_rms.update(rets)
+        return self.transform(reward)
+
+    def transform(self, reward):
+        return th.clamp(
+            reward / th.sqrt(self.ret_rms.var + self.epsilon),
+            -self.cliprew,
+            self.cliprew,
+        )
+
+
+def backward_discounted_sum(
+    *,
+    reward: "(th.Tensor[1, float]) reward",
+    gamma: "(float)",
+):
+
+    # assert first.dim() == 2
+    nstep = reward.shape[0]
+    ret = th.zeros_like(reward)
+    prevret = ret[0] = reward[0]
+    for t in range(1, nstep):
+        prevret = ret[t] = reward[t] + gamma * prevret
+    return ret
+
+
+
+def discount(x, gamma):
+    return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
+
+
+class Postprocessing:
+    """Constant definitions for postprocessing."""
+
+    ADVANTAGES = "advantages"
+    VALUE_TARGETS = "value_targets"
+
+
+reward_normalizer = RewardNormalizer(1)
+
+@DeveloperAPI
+def compute_advantages(rollout,
+                       last_r,
+                       gamma=0.9,
+                       lambda_=1.0,
+                       use_gae=True,
+                       use_critic=True):
+    """
+    Given a rollout, compute its value targets and the advantage.
+
+    Args:
+        rollout (SampleBatch): SampleBatch of a single trajectory
+        last_r (float): Value estimation for last observation
+        gamma (float): Discount factor.
+        lambda_ (float): Parameter for GAE
+        use_gae (bool): Using Generalized Advantage Estimation
+        use_critic (bool): Whether to use critic (value estimates). Setting
+                           this to False will use 0 as baseline.
+
+    Returns:
+        SampleBatch (SampleBatch): Object with experience from rollout and
+            processed rewards.
+    """
+
+    traj = {}
+    trajsize = len(rollout[SampleBatch.ACTIONS])
+
+    for key in rollout:
+        traj[key] = np.stack(rollout[key])
+
+    assert SampleBatch.VF_PREDS in rollout or not use_critic, \
+        "use_critic=True but values not found"
+    assert use_critic or not use_gae, \
+        "Can't use gae without using a value function"
+
+    if use_gae:
+
+        traj[SampleBatch.REWARDS] = reward_normalizer(th.from_numpy(traj[SampleBatch.REWARDS])).numpy()
+
+        vpred_t = np.concatenate(
+            [rollout[SampleBatch.VF_PREDS],
+             np.array([last_r])])
+        delta_t = (
+            traj[SampleBatch.REWARDS] + gamma * vpred_t[1:] - vpred_t[:-1])
+        # This formula for the advantage comes from:
+        # "Generalized Advantage Estimation": https://arxiv.org/abs/1506.02438
+        traj[Postprocessing.ADVANTAGES] = discount(delta_t, gamma * lambda_)
+        traj[Postprocessing.VALUE_TARGETS] = (
+            traj[Postprocessing.ADVANTAGES] +
+            traj[SampleBatch.VF_PREDS]).copy().astype(np.float32)
+    else:
+        rewards_plus_v = np.concatenate(
+            [rollout[SampleBatch.REWARDS],
+             np.array([last_r])])
+        discounted_returns = discount(rewards_plus_v,
+                                      gamma)[:-1].copy().astype(np.float32)
+
+        if use_critic:
+            traj[Postprocessing.
+                 ADVANTAGES] = discounted_returns - rollout[SampleBatch.
+                                                            VF_PREDS]
+            traj[Postprocessing.VALUE_TARGETS] = discounted_returns
+        else:
+            traj[Postprocessing.ADVANTAGES] = discounted_returns
+            traj[Postprocessing.VALUE_TARGETS] = np.zeros_like(
+                traj[Postprocessing.ADVANTAGES])
+
+    traj[Postprocessing.ADVANTAGES] = traj[
+        Postprocessing.ADVANTAGES].copy().astype(np.float32)
+
+    assert all(val.shape[0] == trajsize for val in traj.values()), \
+        "Rollout stacked incorrectly!"
+    return SampleBatch(traj)
---
+++ algorithms/ppg_torch/custom_sgd.py
@@ -0,0 +1,186 @@
+"""Utils for minibatch SGD across multiple RLlib policies."""
+
+import numpy as np
+import logging
+from collections import defaultdict
+import random
+import torch as th
+from torch import distributions as td
+import itertools
+from torch import nn
+
+from ray.util import log_once
+from ray.rllib.evaluation.metrics import LEARNER_STATS_KEY
+from ray.rllib.policy.sample_batch import SampleBatch, DEFAULT_POLICY_ID, \
+    MultiAgentBatch
+from ray.rllib.policy.rnn_sequencing import pad_batch_to_sequences_of_same_size
+
+from . import torch_util as tu
+from .custom_postprocessing import Postprocessing
+
+
+logger = logging.getLogger(__name__)
+
+
+def averaged(kv, axis=None):
+    """Average the value lists of a dictionary.
+
+    For non-scalar values, we simply pick the first value.
+
+    Arguments:
+        kv (dict): dictionary with values that are lists of floats.
+
+    Returns:
+        dictionary with single averaged float as values.
+    """
+    out = {}
+    for k, v in kv.items():
+        if v[0] is not None and not isinstance(v[0], dict):
+            out[k] = np.mean(v, axis=axis)
+        else:
+            out[k] = v[0]
+    return out
+
+
+def standardized(array):
+    """Normalize the values in an array.
+
+    Arguments:
+        array (np.ndarray): Array of values to normalize.
+
+    Returns:
+        array with zero mean and unit standard deviation.
+    """
+    return (array - array.mean()) / max(1e-4, array.std())
+
+
+def minibatches(samples, sgd_minibatch_size):
+    """Return a generator yielding minibatches from a sample batch.
+
+    Arguments:
+        samples (SampleBatch): batch of samples to split up.
+        sgd_minibatch_size (int): size of minibatches to return.
+
+    Returns:
+        generator that returns mini-SampleBatches of size sgd_minibatch_size.
+    """
+    if not sgd_minibatch_size:
+        yield samples
+        return
+
+    if isinstance(samples, MultiAgentBatch):
+        raise NotImplementedError(
+            "Minibatching not implemented for multi-agent in simple mode")
+
+    if "state_in_0" in samples.data:
+        if log_once("not_shuffling_rnn_data_in_simple_mode"):
+            logger.warning("Not shuffling RNN data for SGD in simple mode")
+    else:
+        samples.shuffle()
+
+    i = 0
+    slices = []
+    while i < samples.count:
+        slices.append((i, i + sgd_minibatch_size))
+        i += sgd_minibatch_size
+    random.shuffle(slices)
+
+    for i, j in slices:
+        yield samples.slice(i, j)
+
+
+nepochs = 0
+seg_buf = []
+
+
+def do_minibatch_sgd(samples, policies, local_worker, num_sgd_iter,
+                     sgd_minibatch_size, standardize_fields):
+    """Execute minibatch SGD.
+
+    Arguments:
+        samples (SampleBatch): batch of samples to optimize.
+        policies (dict): dictionary of policies to optimize.
+        local_worker (RolloutWorker): master rollout worker instance.
+        num_sgd_iter (int): number of epochs of optimization to take.
+        sgd_minibatch_size (int): size of minibatches to use for optimization.
+        standardize_fields (list): list of sample field names that should be
+            normalized prior to optimization.
+
+    Returns:
+        averaged info fetches over the last SGD epoch taken.
+    """
+    # Get batch
+    global nepochs
+    global seg_buf
+    if isinstance(samples, SampleBatch):
+        samples = MultiAgentBatch({DEFAULT_POLICY_ID: samples}, samples.count)
+    
+    fetches = {}
+    for policy_id, policy in policies.items():
+        model = policy.model
+
+        dist_class = policy.dist_class
+
+        if policy_id not in samples.policy_batches:
+            continue
+
+        batch = samples.policy_batches[policy_id]
+        for field in standardize_fields:
+            batch[field] = standardized(batch[field])
+
+        seg_buf.append(batch)
+
+        for i in range(num_sgd_iter):
+            iter_extra_fetches = defaultdict(list)
+            #pass the whole batch to the worker, then let it break it down into minibatches
+            batch_fetches = (local_worker.learn_on_batch(
+                MultiAgentBatch({
+                    policy_id: batch
+                }, batch.count)))[policy_id]
+
+            for k, v in batch_fetches.get(LEARNER_STATS_KEY, {}).items():
+                iter_extra_fetches[k].append(v)
+
+            logger.debug("{} {}".format(i, averaged(iter_extra_fetches)))
+
+        fetches[policy_id] = averaged(iter_extra_fetches)
+    
+        nepochs += 1
+        if nepochs % 16 == 0:
+            def forward(seg):
+                logits, state = model.from_batch(seg)
+                return logits, state      
+
+            REPLAY_MB_SIZE = 256
+            # #compute presleep outputs for replay buffer (what does this mean?)
+            for seg in seg_buf:
+                np_data = {}
+                np_data["obs"] = th.from_numpy(seg.data["obs"]).to(th.cuda.current_device())
+                logits, state = tu.minibatched_call(forward, REPLAY_MB_SIZE, seg=np_data)
+                seg.data["oldpd"] = logits.cpu().numpy()
+
+            replay_batch = SampleBatch.concat_samples(seg_buf)
+            #train on replay buffer
+            for i in range(3):   
+                for mb in minibatches(replay_batch, REPLAY_MB_SIZE):
+                    pad_batch_to_sequences_of_same_size(mb,
+                                                        max_seq_len=20,
+                                                        shuffle=False,
+                                                        batch_divisibility_req=1)
+                    mb["obs"] = th.from_numpy(mb["obs"]).to(th.cuda.current_device())
+
+                    logits, vpredaux = model.forward_aux(mb)
+
+                    oldpd = dist_class(th.from_numpy(mb['oldpd']).to(th.cuda.current_device()))
+                    pd = dist_class(logits, model)
+                    pol_distance = oldpd.kl(pd).mean()
+
+                    vpredtrue = model.value_function()
+                    vtarg = th.from_numpy(mb[Postprocessing.VALUE_TARGETS]).to(th.cuda.current_device())
+                    vf_aux = 0.5 * th.mean(th.pow(vpredaux - vtarg, 2.0))
+                    vf_true = 0.5 * th.mean(th.pow(vpredtrue - vtarg, 2.0))
+                    loss = pol_distance + vf_aux + vf_true
+
+                    policy.aux_learn(loss)
+            seg_buf.clear()
+    return fetches
