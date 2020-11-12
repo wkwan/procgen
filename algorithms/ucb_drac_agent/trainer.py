2a1ed8b49a003b983b5dacbc84dc14632dc18fbc
William Kwan <wkwan94@gmail.com>
2020-11-12 16:23:30 -0500

    fix default config

--- algorithms/ucb_drac_agent/trainer.py
+++ algorithms/ucb_drac_agent/trainer.py
@@ -1,7 +1,7 @@
 import logging
 
 from ray.rllib.agents import with_common_config
-from .ucb_ppo_policy import PPOTorchPolicy
+from .ucb_ppo_policy import PPOTorchPolicy, DEFAULT_CONFIG
 from ray.rllib.agents.trainer_template import build_trainer
 from ray.rllib.execution.rollout_ops import ParallelRollouts, ConcatBatches, \
     StandardizeFields, SelectExperiences
@@ -13,71 +13,7 @@ tf = try_import_tf()
 
 logger = logging.getLogger(__name__)
 
-# yapf: disable
-# __sphinx_doc_begin__
-DEFAULT_CONFIG = with_common_config({
-    # Should use a critic as a baseline (otherwise don't use value baseline;
-    # required for using GAE).
-    "use_critic": True,
-    # If true, use the Generalized Advantage Estimator (GAE)
-    # with a value function, see https://arxiv.org/pdf/1506.02438.pdf.
-    "use_gae": True,
-    # The GAE(lambda) parameter.
-    "gamma": 0.999,
-    "lambda": 0.95,
-    # Initial coefficient for KL divergence.
-    "kl_coeff": 0.0,
-    # Size of batches collected from each worker.
-    "rollout_fragment_length": 256,
-    # Number of timesteps collected for each SGD round. This defines the size
-    # of each SGD epoch.
-    "train_batch_size": 8192,
-    # Total SGD batch size across all devices for SGD. This defines the
-    # minibatch size within each epoch.
-    "sgd_minibatch_size": 1024,
-    # Whether to shuffle sequences in the batch when training (recommended).
-    "shuffle_sequences": True,
-    # Number of SGD iterations in each outer loop (i.e., number of epochs to
-    # execute per train batch).
-    "num_sgd_iter": 3,
-    # Stepsize of SGD.
-    "lr": 5e-4,
-    # Learning rate schedule.
-    "lr_schedule": None,
-    # Share layers for value function. If you set this to True, it's important
-    # to tune vf_loss_coeff.
-    "vf_share_layers": True,
-    # Coefficient of the value function loss. IMPORTANT: you must tune this if
-    # you set vf_share_layers: True.
-    "vf_loss_coeff": 0.5,
-    # Coefficient of the entropy regularizer.
-    "entropy_coeff": 0.01,
-    # Decay schedule for the entropy regularizer.
-    "entropy_coeff_schedule": None,
-    # PPO clip parameter.
-    "clip_param": 0.2,
-    # Clip param for the value function. Note that this is sensitive to the
-    # scale of the rewards. If your expected V is large, increase this.
-    "vf_clip_param": 0.2,
-    # If specified, clip the global norm of gradients by this amount.
-    "grad_clip": 0.5,
-    # Target value for KL divergence.
-    "kl_target": 0.01,
-    # Whether to rollout "complete_episodes" or "truncate_episodes".
-    "batch_mode": "truncate_episodes",
-    # Which observation filter to apply to the observation.
-    "observation_filter": "NoFilter",
-    # Uses the sync samples optimizer instead of the multi-gpu one. This is
-    # usually slower, but you might want to try it if you run into issues with
-    # the default optimizer.
-    "simple_optimizer": False,
-    # Whether to fake GPUs (using CPUs).
-    # Set this to True for debugging on non-GPU machines (set `num_gpus` > 0).
-    "_fake_gpus": False,
-    "framework": "torch"
-})
-# __sphinx_doc_end__
-# yapf: enable
+
 
 
 def warn_about_bad_reward_scales(config, result):
